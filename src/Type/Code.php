c4f5d95298764004dd4b0cc3ca55e33417f22caa
Artem Vasilev <kern.usr@gmial.com>
2024-02-12 11:27:54 +0300

    Добавил комментарии

--- src/Type/Code.php
+++ src/Type/Code.php
@@ -2,9 +2,23 @@
 
 namespace Webmasterskaya\Kladr\Type;
 
+/**
+ * Допустимые типы населённых пунктов
+ */
 final class Code
 {
+    /**
+     * Город
+     */
     public const CITY = 1;
+
+    /**
+     * Село
+     */
     public const VILLAGE = 2;
+
+    /**
+     * Деревня
+     */
     public const RURAL = 4;
 }
--- src/Type/Content.php
+++ src/Type/Content.php
@@ -2,11 +2,33 @@
 
 namespace Webmasterskaya\Kladr\Type;
 
+/**
+ * Допустимые типы полей адреса
+ */
 class Content
 {
+    /**
+     * Регион
+     */
     public const REGION = 'region';
+
+    /**
+     * Район
+     */
     public const DISTRICT = 'district';
+
+    /**
+     * Населённый пункт
+     */
     public const CITY = 'city';
+
+    /**
+     * Улица
+     */
     public const STREET = 'street';
+
+    /**
+     * Дом
+     */
     public const BUILDING = 'building';
 }
